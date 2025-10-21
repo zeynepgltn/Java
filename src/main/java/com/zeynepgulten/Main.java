d4d8553a200c383caa8a9a8a930a8b5eb64e6917
Zeynep Gülten <168850598+zeynepgltn@users.noreply.github.com>
2025-10-21 16:25:16 +0300

    Java Class Created

--- src/main/java/com/zeynepgulten/Main.java
+++ src/main/java/com/zeynepgulten/Main.java
@@ -1,6 +1,8 @@
+package com.zeynepgulten;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome !");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
---
+++ src/main/java/com/zeynepgulten/_1_part_javatypes/_01_JavaTypes.java
@@ -0,0 +1,6 @@
+package com.zeynepgulten._1_part_javatypes;
+
+public class _01_JavaTypes {
+
+
+}
